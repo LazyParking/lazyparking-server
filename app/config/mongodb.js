97f4af611750d8c1e420fe264db81e56318f4222
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-01-08 20:19:53 -0200

    Move CoffeeScript files to app
    - Add coffee-script/register require
    - delete compiled js files

--- app/config/mongodb.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  user: process.env.MONGODB_USER || '',
-  pass: process.env.MONGODB_PASS || '',
-  server: process.env.MONGODB_SERVER || 'localhost',
-  port: process.env.MONGODB_PORT || 27017,
-  name: process.env.MONGODB_NAME || 'lazypark'
-};
